cbcbfc4f88d6bbf86eef391b076b45767fc37482
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-08-12 14:40:46 +0530

    Auto Push

--- docdb.tf
+++ docdb.tf
@@ -11,5 +11,22 @@ resource "aws_docdb_cluster" "docdb" {
 #   preferred_backup_window = "02:00-05:00"
 }
 
+# creates subnet group 
+resource "aws_docdb_subnet_group" "docdb_subnet_group" {
+  name       = "roboshop-docdb-${var.ENV}-subnetgroup"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
 
+  tags = {
+    Name = "roboshop-docdb-${var.ENV}-subnetgroup"
+  }
+}
 
+# Creates Compute Machines needed for DocumentDB and these has to be attached to the cluster
+resource "aws_docdb_cluster_instance" "cluster_instances" {
+  count              = var.DOCDB_INSTANCE_COUNT
+  identifier         = "roboshop-${var.ENV}-docdb-instance"
+  cluster_identifier = aws_docdb_cluster.docdb.id                      #This argument attaches the nodes created here to the docdb cluster.
+  instance_class     = var.DOCDB_INSTANCE_TYPE
+
+  depends_on = [aws_docdb_cluster.docdb]
+}

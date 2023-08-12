662e36b2fa63b2580af5d6685e4a10a123a9be2b
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-08-12 14:29:11 +0530

    Auto Push

---
+++ data.tf
@@ -0,0 +1,23 @@
+# Terraform Renote State Datasource : This is going to read the data from another statefile
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "b54-tf-remote-stat"
+    key    = "vpc/${var.ENV}/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "aws_secretsmanager_secret" "secrets" {
+  name = "robot/secrets"
+}
+
+
+data "aws_secretsmanager_secret_version" "secret_version" {
+  secret_id = data.aws_secretsmanager_secret.secrets.id
+}
+
+# output "example" {
+#   value = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["DOCDB_USERNAME"]
+# }

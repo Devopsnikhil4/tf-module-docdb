aaad4a130df54187883fbcc478fc28b586f3c8ef
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-08-12 15:31:01 +0530

    Auto Push

--- docdb.tf
+++ docdb.tf
@@ -1,8 +1,8 @@
 resource "aws_docdb_cluster" "docdb" {
   cluster_identifier       = "roboshop-${var.ENV}-docdb"
   engine                   = "docdb" 
-  master_username          = local.DOCDB_USER
-  master_password          = local.DOCDB_PASS
+  master_username          = "admin1"
+  master_password          = "roboshop1"
   skip_final_snapshot      = true  
   vpc_security_group_ids   = [aws_security_group.allows_docdb.id]
   db_subnet_group_name     = aws_docdb_subnet_group.docdb_subnet_group.name
